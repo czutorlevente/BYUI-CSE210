22a31e7334806b1e396993dabe5f171baebb0334
unknown <czutorlevente@gmail.com>
2023-09-16 22:41:00 -0600

    Prep 3 is done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        bool guessedRight = false;
+
+        do
+        {
+
+            Console.Write("What is your guess? ");
+            string prompt2 = Console.ReadLine();
+            int guessNumber = int.Parse(prompt2);
+
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed right!");
+                guessedRight = true;
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guessedRight == false);
     }
 }
