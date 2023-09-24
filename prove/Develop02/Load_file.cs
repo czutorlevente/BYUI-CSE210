00b5fa9893413fb76c409b18bbef9a274c9d347b
unknown <czutorlevente@gmail.com>
2023-09-23 22:31:14 -0600

    Program is improving

--- prove/Develop02/Load_file.cs
+++ prove/Develop02/Load_file.cs
@@ -3,9 +3,10 @@ using System;
 public class Load_file
 {
 
-    public string [] GetLines(string filename)
+    public List<string> GetLines(string filename)
     {
-        string [] lines = System.IO.File.ReadAllLines(filename);
+        string [] lines_1 = System.IO.File.ReadAllLines(filename);
+        List<string> lines = lines_1.ToList();
         return lines;
     }
 
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,12 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Load_file load_1 = new Load_file();
-        string [] lines_1 = load_1.GetLines("Journal.txt");
+        List<string> lines_1 = load_1.GetLines("Journal.txt");
 
         Entry entry = new Entry();
 
 
         string entry_1 = entry.Prompt();
+        lines_1.Add(entry_1);
 
         foreach (string line in lines_1)
         {
