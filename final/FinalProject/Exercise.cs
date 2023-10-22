1706eee49e5a2ddd845ff353dfd9d8c6fd030690
unknown <czutorlevente@gmail.com>
2023-10-21 23:52:00 -0600

    Classes are made

---
+++ final/FinalProject/Exercise.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Exercise
+{
+    private string _name;
+    private string _descripton;
+    private int _points;
+
+    public Exercise(string name, string description, int points)
+    {
+        _name = name;
+        _descripton = description;
+        _points = points;
+    }
+}
