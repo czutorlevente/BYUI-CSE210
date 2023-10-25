ed6ed88679717d6562e2ec860f5489eba57046cc
unknown <czutorlevente@gmail.com>
2023-10-24 23:29:50 -0600

    Done! 3.0

--- final/FinalProject/Motivation.cs
+++ final/FinalProject/Motivation.cs
@@ -15,7 +15,8 @@ class Motivation
         "Your body is a temple, take good care of it!",
         "Having a physical body is a privilege. Show your gratitude for it!",
         "Your can do all things through His strenght!",
-        "Self-control is the basis of self-esteem"
+        "Self-control is the basis of self-esteem",
+        "Run in such a way as to get the prize!"
     };
     List<string> spinlist = new List<string>{"|", "/", "-", "\\"};
     public void ShowSpinner(int seconds)
